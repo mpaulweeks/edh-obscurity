b63cc6bb62e2c6d155ab33b27acd255bd454b837
mpaulweeks <mpaulweeks@gmail.com>
2017-06-04 15:14:00 -0400

    deploy.sh

---
+++ bash/deploy.sh
@@ -0,0 +1,13 @@
+cd quiz-app
+
+npm run build
+mkdir ../docs
+rm -r ../docs
+mkdir ../docs
+cp -a build/ ../docs
+# echo 'edh-obscurity.mpaulweeks.com' > ../docs/CNAME
+
+cd ..
+git add .
+git commit -m 'deploy'
+git push
