3481d6caed75d551f65910ae368d1f839ce2a87e
mpaulweeks <mpaulweeks@gmail.com>
2017-06-06 00:04:44 -0400

    preserve existing CNAME

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,11 +1,16 @@
 cd edh-obscurity-app
 
 npm run build
+
 mkdir ../docs
+touch ../docs/CNAME
+cp ../docs/CNAME CNAME.temp
 rm -r ../docs
+
 mkdir ../docs
 cp -a build/ ../docs
-echo 'edh-obscurity.mpaulweeks.com' > ../docs/CNAME
+cp CNAME.temp ../docs/CNAME
+rm CNAME.temp
 cp README_BUILD.md ../docs/README.md
 
 cd ..
